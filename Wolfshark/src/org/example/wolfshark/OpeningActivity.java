0962e74b41d91a36d63e1a03aa4cf5aecc26d935
Jolie Nazor <jolie.nazor@gmail.com>
2013-02-02 16:15:32 -0800

    Created for displaying the opening screen for 3 seconds.

---
+++ Wolfshark/src/org/example/wolfshark/OpeningActivity.java
@@ -0,0 +1,36 @@
+package org.example.wolfshark;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class OpeningActivity extends Activity {
+
+	//Don't use this class as a good example!
+	//I feel there should be a better way to do this
+	//but found this on theNewBoston's tutorial
+	//and it should work.
+	//The use of try, catch, and finally is a good example
+	//of Java programming.
+	//Also has an Intent example
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);		//have to have this
+		setContentView(R.layout.activity_open);    //have to have this
+		Thread mythread = new Thread(){
+			public void run(){
+				try{
+					sleep(3000);    //3000 milliseconds == 3 seconds
+				}catch(InterruptedException e){
+					//print to console, not to Android device
+					System.out.println("Error occurred on Opening Screen.");
+				}finally{
+					//Use this style of Intent, 
+					//     not the one shown in theNewBoston Tutorial 15
+					Intent openMain = new Intent(OpeningActivity.this, MainActivity.class);
+					startActivity(openMain);
+				}
+			}
+		};
+		mythread.start();
+	}	
+}
