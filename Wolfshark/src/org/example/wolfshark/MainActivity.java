6312b81004121783eb755f974b862e470e0ac527
lloudermilk <laurynloudermilk@gmail.com>
2013-02-12 21:39:04 -0800

    Merge fix.
    Because merging is tough, man.

--- Wolfshark/src/org/example/wolfshark/MainActivity.java
+++ Wolfshark/src/org/example/wolfshark/MainActivity.java
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-package org.example.wolfshark;
-
-import android.os.Bundle;
-import android.view.View;
-import android.app.Activity;
-import android.content.Intent;
-
-public class MainActivity extends Activity {
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	public void onRoute(View v)
-	{
-		Intent locateActivity = new Intent(this, LocateInputActivity.class);
-		startActivity(locateActivity);
-	}
-	
-	
-	public void onFavorites(View v)
-	{
-		Intent favoritesActivity = new Intent (this, FavoritesActivity.class);
-		startActivity(favoritesActivity);
-	}
-	
-	public void onMap(View v)
-	{
-		Intent mapView = new Intent (this, MapActivity.class);
-		startActivity(mapView);
-	}
-	
-	public void onAbout(View v)
-	{
-		Intent AboutActivity = new Intent (this, AboutActivity.class);
-		startActivity(AboutActivity);
-	}
-}
-=======
 package org.example.wolfshark;
 
 import android.os.Bundle;
@@ -73,4 +31,3 @@ public class MainActivity extends Activity {
 		startActivity(AboutActivity);
 	}
 }
->>>>>>> testBranch
