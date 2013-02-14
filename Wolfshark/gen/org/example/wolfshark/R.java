32c9a59322d482031b8891283bd4ec4f3195a6c8
micah <sonicemerald@gmail.com>
2013-02-14 12:46:00 -0800

    got the spinner working on the route page
    haven't linked anything up, but i also played around with the
    autocomplete text... my thinking is with autocomplete, it could act just
    like a spinner, (we'd just have to set it to automatically show the
    list) yet easier to navigate (with letters)

--- Wolfshark/gen/org/example/wolfshark/R.java
+++ Wolfshark/gen/org/example/wolfshark/R.java
@@ -8,40 +8,45 @@
 package org.example.wolfshark;
 
 public final class R {
+    public static final class array {
+        public static final int buildings_array=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int background=0x7f040000;
-        public static final int gray=0x7f040001;
-        public static final int white=0x7f040002;
+        public static final int background=0x7f050000;
+        public static final int gray=0x7f050001;
+        public static final int white=0x7f050002;
     }
     public static final class drawable {
         public static final int darwin_basement=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int Start_text=0x7f080008;
-        public static final int about=0x7f080012;
-        public static final int about_content=0x7f080000;
-        public static final int autoCompleteTextView1=0x7f080009;
-        public static final int autoCompleteTextView2=0x7f08000a;
-        public static final int dar_basement=0x7f080013;
-        public static final int editFavs=0x7f080007;
-        public static final int endFav=0x7f08000c;
-        public static final int favorites=0x7f080011;
-        public static final int favs1=0x7f080001;
-        public static final int favs2=0x7f080002;
-        public static final int favs3=0x7f080003;
-        public static final int favs4=0x7f080004;
-        public static final int favs5=0x7f080005;
-        public static final int locateInput=0x7f08000e;
-        public static final int map=0x7f080010;
-        public static final int newPath=0x7f080014;
-        public static final int route=0x7f08000f;
-        public static final int saveFavs=0x7f080006;
-        public static final int settings=0x7f080015;
-        public static final int startFav=0x7f08000b;
-        public static final int starttext=0x7f08000d;
+        public static final int Start_text=0x7f090009;
+        public static final int about=0x7f090014;
+        public static final int about_content=0x7f090000;
+        public static final int building_start=0x7f09000a;
+        public static final int dar_basement=0x7f090015;
+        public static final int editFavs=0x7f090007;
+        public static final int endFav=0x7f090010;
+        public static final int end_building=0x7f09000e;
+        public static final int end_room=0x7f09000f;
+        public static final int endtext=0x7f090008;
+        public static final int favorites=0x7f090013;
+        public static final int favs1=0x7f090001;
+        public static final int favs2=0x7f090002;
+        public static final int favs3=0x7f090003;
+        public static final int favs4=0x7f090004;
+        public static final int favs5=0x7f090005;
+        public static final int locateInput=0x7f09000c;
+        public static final int map=0x7f090012;
+        public static final int newPath=0x7f090016;
+        public static final int route=0x7f090011;
+        public static final int saveFavs=0x7f090006;
+        public static final int settings=0x7f090017;
+        public static final int startFav=0x7f09000b;
+        public static final int start_room=0x7f09000d;
     }
     public static final class layout {
         public static final int activity_about=0x7f030000;
@@ -53,24 +58,24 @@ public final class R {
         public static final int activity_open=0x7f030006;
     }
     public static final class menu {
-        public static final int menu=0x7f070000;
+        public static final int menu=0x7f080000;
     }
     public static final class string {
-        public static final int about=0x7f050006;
-        public static final int about_text=0x7f05000e;
-        public static final int app_name=0x7f050000;
-        public static final int darBase=0x7f050008;
-        public static final int darRmDest=0x7f050009;
-        public static final int edit_favs=0x7f05000c;
-        public static final int favorites=0x7f050004;
-        public static final int favs_title=0x7f05000b;
-        public static final int go=0x7f050003;
-        public static final int main_title=0x7f050001;
-        public static final int map=0x7f050005;
-        public static final int newPath=0x7f05000a;
-        public static final int route=0x7f050002;
-        public static final int save_favs=0x7f05000d;
-        public static final int temp=0x7f050007;
+        public static final int about=0x7f060006;
+        public static final int about_text=0x7f06000e;
+        public static final int app_name=0x7f060000;
+        public static final int darBase=0x7f060008;
+        public static final int darRmDest=0x7f060009;
+        public static final int edit_favs=0x7f06000c;
+        public static final int favorites=0x7f060004;
+        public static final int favs_title=0x7f06000b;
+        public static final int go=0x7f060003;
+        public static final int main_title=0x7f060001;
+        public static final int map=0x7f060005;
+        public static final int newPath=0x7f06000a;
+        public static final int route=0x7f060002;
+        public static final int save_favs=0x7f06000d;
+        public static final int temp=0x7f060007;
     }
     public static final class style {
         /** 
@@ -94,10 +99,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- Wolfshark/src/org/example/wolfshark/LocateInputActivity.java
+++ Wolfshark/src/org/example/wolfshark/LocateInputActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+//import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 
 public class LocateInputActivity extends Activity {
 	
@@ -11,9 +14,34 @@ public class LocateInputActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_locateinput);
 		System.out.println("LocateInputActivity Started.");
-	}
 	
-	public void onGo(View v)
+		/*
+		//code for AutoCompletetext.
+		// Get a reference to the AutoCompleteTextView in the layout
+		AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
+		// Get the string array
+		String[] buildings = getResources().getStringArray(R.array.buildings_array);
+		// Create the adapter and set it to the AutoCompleteTextView 
+		ArrayAdapter<String> adapter = 
+	        new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, buildings);
+		textView.setAdapter(adapter);
+		//end autocomplete	*/
+	
+		//Spinner code.
+		Spinner spinner = (Spinner) findViewById(R.id.building_start);
+		Spinner spinner2 = (Spinner) findViewById(R.id.end_building);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> spinadapter = ArrayAdapter.createFromResource(this,
+		        R.array.buildings_array, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		spinadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(spinadapter);
+		spinner2.setAdapter(spinadapter);
+	
+	}
+    
+    public void onGo(View v)
 	{
 		Intent myMapPath = new Intent(this, MapPathActivity.class);
 		startActivity(myMapPath);
