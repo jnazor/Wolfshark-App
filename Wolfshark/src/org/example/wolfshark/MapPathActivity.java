e3374e0f2bd877b269e069fc3abc881bc6b3496c
Jolie Nazor <jolie.nazor@gmail.com>
2013-02-17 18:55:03 -0800

    Now receives values from LocateInputActivity.java and displays them on the screen.

--- Wolfshark/src/org/example/wolfshark/MapPathActivity.java
+++ Wolfshark/src/org/example/wolfshark/MapPathActivity.java
@@ -2,9 +2,12 @@ package org.example.wolfshark;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 //Idea for drawing on the map comes from:
 //http://nxsfan.co.uk/blog/2010/06/18/ondraw-drawing-a-simple-line-on-a-background/
@@ -12,6 +15,15 @@ import android.widget.LinearLayout;
 public class MapPathActivity extends Activity {
 	
 	LinearLayout PathLayout;
+	TextView testText;
+	String start_build;
+	String end_build;
+	String start_room;
+	String end_room;
+	
+	public static final String PREFERENCE_FILENAME = "LocatePrefs";
+    private SharedPreferences myPrefs;
+    SharedPreferences.Editor myEditor;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,6 +32,25 @@ public class MapPathActivity extends Activity {
 		PathLayout = (LinearLayout) findViewById(R.id.dar_basement);
 		CustomView myView = new CustomView(this);
 		PathLayout.addView(myView);
+		
+		testText = (TextView) findViewById(R.id.testOutput);
+		testText.setTextColor(Color.BLUE);
+		
+		
+		myPrefs = getSharedPreferences(PREFERENCE_FILENAME, MODE_PRIVATE);
+        myEditor = myPrefs.edit();
+		
+		start_build = "";
+		end_build = "";
+		start_room = "";
+		end_room = "";
+		
+		start_build = myPrefs.getString("startBuild", "None");
+		end_build = myPrefs.getString("endBuild", "None");
+		start_room = myPrefs.getString("startRoom", "None");
+		end_room = myPrefs.getString("endRoom", "None");
+		
+		testText.setText("Test: " + start_build + " " + start_room + "\n      " + end_build + " " + end_room);
 		
 	}
 	
