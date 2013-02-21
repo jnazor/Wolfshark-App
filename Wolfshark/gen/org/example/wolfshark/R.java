26eb6b85a45198f5ee012e590f7e01597089592f
Jolie Nazor <jolie.nazor@gmail.com>
2013-02-21 00:22:27 -0800

    Changed during testing

--- Wolfshark/gen/org/example/wolfshark/R.java
+++ Wolfshark/gen/org/example/wolfshark/R.java
@@ -22,7 +22,10 @@ public final class R {
     public static final class drawable {
         public static final int campus_map=0x7f020000;
         public static final int darwin_basement=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int darwin_first=0x7f020002;
+        public static final int darwin_second=0x7f020003;
+        public static final int darwin_third=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
     }
     public static final class id {
         public static final int Start_text=0x7f090008;
