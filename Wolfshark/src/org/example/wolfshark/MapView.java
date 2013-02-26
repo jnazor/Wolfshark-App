e735c50a3760a636dc6ec4b886918f5878092d89
Jake Hagen <shozknawe@gmail.com>
2013-02-26 01:14:45 -0800

    Added framework for drawing on map, sample path
    Also resized darwin_basement.png

--- Wolfshark/src/org/example/wolfshark/MapView.java
+++ Wolfshark/src/org/example/wolfshark/MapView.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,7 +17,8 @@ public class MapView extends View {
 
 	Paint imgPaint;
 	Bitmap mapGraphic = BitmapFactory.decodeResource(getResources(), R.drawable.darwin_basement);
-	
+	Path thePath;
+	Paint pathPaint;
 
 	int mapX = 0;
 	int mapY = 0;
@@ -36,15 +38,34 @@ public class MapView extends View {
 	public MapView(Context context)
 	{
 		super(context);
-		imgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		this.setBackgroundColor(Color.WHITE);
+		
+		imgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		thePath = new Path();
+		
+		pathPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		pathPaint.setColor(Color.BLACK);
+		pathPaint.setStrokeWidth(3);
+		pathPaint.setStyle(Paint.Style.STROKE);
+		
+
 	}
 	
 	@Override
 	protected void onDraw(Canvas canvas)
 	{
+
 		
 		canvas.drawBitmap(mapGraphic, mapX + offsetX, mapY + offsetY, imgPaint);
+		
+		thePath.reset();
+		
+		thePath.moveTo(250+offsetX+mapX, 250+offsetY+mapY);
+		thePath.lineTo(200+offsetX+mapX, 200+offsetY+mapY);
+		thePath.lineTo(600+offsetX+mapX, 200+offsetY+mapY);
+		thePath.lineTo(300+offsetX+mapX, 1200+offsetY+mapY);
+		canvas.drawPath(thePath, pathPaint);
+		
 	
 	}
 	
